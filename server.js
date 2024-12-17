5070dab20dad872aeb3f62eada8c23e1cbc4d8f1
win877969 <xpandhita@gmail.com>
2024-12-17 18:29:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Welcome to Proxy API</h1>');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404: Not Found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
+
+
 // Fungsi untuk menghasilkan UUID v4 varian 2
 function generateUUIDv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
